0ca341c79c6c598f0345134097e63713940a52bd
Axel Nolasco <axeln@bluetrailsoft.com>
2019-05-27 20:27:42 -0500

    All course made with hooks

---
+++ src/SearchParams.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import userDropdown from "./useDropdown";
+import Results from "./Results";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Tepatitlan, JAL.");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = userDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = userDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+    console.log(animals);
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;
